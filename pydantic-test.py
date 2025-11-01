c1dee03d5fc720a85499a90326a260d00d27408d
Josh Lemberg <josh_lemberg@trimble.com>
2025-11-01 15:48:12 -0400

    pydantic test

---
+++ pydantic-test.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, field_validator
+from typing import Literal
+
+class User(BaseModel):
+    idnum: int
+    # username: str
+
+    # @field_validator("username", mode="after")
+    # @classmethod
+    # def is_josh(cls, value: str) -> str:
+    #     if value != 'josh':
+    #         raise ValueError(f'idek')
+    #     return value
+
+
+
+u = User(idnum=8, username='josh')
